d13e9054f4dad8fa1b808f8c452299496804ed81
Shubhraneel Gupta <129332238+ShubhraneelGupta@users.noreply.github.com>
2025-12-25 23:33:21 +0530

    Switching from GPU backend to CPU

--- iv.c
+++ iv.c
@@ -22,33 +22,59 @@ int main(void)
         return 1;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-        window,
-        -1,
-        SDL_RENDERER_ACCELERATED
-    );
-    if (!renderer) {
-        fprintf(stderr, "CreateRenderer failed: %s\n", SDL_GetError());
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+    if(!surface){
+        fprintf(stderr, "Getting window surface failed: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
+//GPU WORKFLOW
+    // SDL_Renderer *renderer = SDL_CreateRenderer(
+    //     window,
+    //     -1,
+    //     SDL_RENDERER_ACCELERATED
+    // );
+    // if (!renderer) {
+    //     fprintf(stderr, "CreateRenderer failed: %s\n", SDL_GetError());
+    //     SDL_DestroyWindow(window);
+    //     SDL_Quit();
+    //     return 1;
+    // }
+
+    // Uint32 start = SDL_GetTicks();
+    // while (SDL_GetTicks() - start < 5000) {
+    //     SDL_Event event;
+    //     while (SDL_PollEvent(&event)) {
+    //         /* polling only */
+    //     }
+
+    //     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    //     SDL_RenderClear(renderer);
+    //     SDL_RenderPresent(renderer);
+
+    //     SDL_Delay(16);
+    // }
+
+    //SDL_DestroyRenderer(renderer);
+
+
+//CPU WORKFLOW
+    SDL_FillRect(
+        surface,
+        NULL,
+        0xFFFFFF
+    );
+
+    SDL_UpdateWindowSurface(window);
 
     Uint32 start = SDL_GetTicks();
-    while (SDL_GetTicks() - start < 5000) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            /* polling only */
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-
+    while (SDL_GetTicks() - start < 2000) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {}
         SDL_Delay(16);
     }
 
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
