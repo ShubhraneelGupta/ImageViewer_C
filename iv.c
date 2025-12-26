1219558a0e0b970361184d883f96030dd78da452
Shubhraneel Gupta <129332238+ShubhraneelGupta@users.noreply.github.com>
2025-12-26 14:47:05 +0530

    Added parsing for P6 PPM
    -- now it can parse any p6 ppm image and display
    
    -- press 'q' to quit :))

--- iv.c
+++ iv.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[])
@@ -36,31 +37,59 @@ int main(int argc, char *argv[])
 
 
 //PPM
-    image = fopen(argv[1], "r");
+    //Open ppm file
+    image = fopen(argv[1], "rb");
     if (image == NULL) 
     {
         perror("fopen");
         return 1;
     }
-    //Finding height and width
-    char height[8];
-    char width[8];
 
-    //Height and width are in 3rd line for ppm files
-    char buf[256]; int line = 0;
-    while(fgets(buf, sizeof buf, image))
+    //Read PPM header
+    char magic[3];
+    int width = 0, height = 0, maxval = 0;
+
+    //Read Magic number
+    if(fscanf(image, "%2s", magic) != 1)
     {
-        line++;
-        if(line == 3)
-        {
-            sscanf(buf, "%s %s", &width, &height);
-            break;
-        }
+        fprintf(stderr, "Invalid PPM file\n");
+        fclose(image);
+        return 1;
+    }
+    // Check if it's P6 format (binary PPM)
+    if (strcmp(magic, "P6\0") != 0) {
+        fprintf(stderr, "Only P6 format PPM files are supported\n");
+        fclose(image);
+        return 1;
     }
 
-    //converting height and width to int
-    const int HEIGHT = atoi(height);
-    const int WIDTH = atoi(width);
+    //Skip whitespace and comments
+    int c;
+    while((c = fgetc(image)) != EOF && isspace(c));
+
+    //Handle comments
+    while(c == '#')
+    {
+        while((c = fgetc(image)) != EOF && c != '\n');
+        while((c = fgetc(image)) != EOF && isspace(c));
+    }
+
+    //Put back the non-whitespace character
+    ungetc(c, image);
+
+    //Read width, height, and maxval
+    if(fscanf(image, "%d %d %d", &width, &height, &maxval) != 3)
+    {
+        fprintf(stderr, "Invalid PPM header\n");
+        fclose(image);
+        return 1;
+    }
+
+    //Skip the whitespace after the maxval chaacter
+    fgetc(image);
+
+    const int HEIGHT = height;
+    const int WIDTH = width;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) 
     {
@@ -71,7 +100,7 @@ int main(int argc, char *argv[])
 
     //Window creation.
     SDL_Window *window = SDL_CreateWindow(
-        "Test Window",
+        "Image Viewer",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         WIDTH,
@@ -123,36 +152,59 @@ int main(int argc, char *argv[])
 
 
 //CPU WORKFLOW
-    Uint8 r, g, b;
     SDL_Rect pixel = (SDL_Rect)
     {
             0, 0,
             1, 1
     };
-    for(int x = 0; x < WIDTH; x++)
+    for(int y = 0; y < HEIGHT; y++)
     {
-        for(int y = 0; y < HEIGHT; y++)
+        for(int x = 0; x < WIDTH; x++)
         {
-            Uint32 color = SDL_MapRGB(surface->format, (x+y)%255, (x*y)%255, (x*x)%255);
-            pixel.x = x; pixel.y = y;
-            SDL_FillRect(
-                surface,
-                &pixel,
-                color
-            );
+            Uint8 r = fgetc(image);
+            Uint8 g = fgetc(image);
+            Uint8 b = fgetc(image);
+
+            //If maxval is not 255
+            if(maxval != 255)
+            {
+                r = (r*255) / maxval;
+                g = (g*255) / maxval;
+                b = (b*255) / maxval;
+            }
+
+            Uint32 color = SDL_MapRGB(surface->format, r, g, b);
+
+            pixel.x = x;
+            pixel.y = y;
+
+            SDL_FillRect(surface, &pixel, color);
         }
     }
     SDL_UpdateWindowSurface(window);
 
     // Event polling is required on Wayland to keep the surface mapped.
     // on X11 the window may still appear without it, but polling is still recommended.
-    Uint32 start = SDL_GetTicks();
-    while (SDL_GetTicks() - start < 2000) 
+    SDL_bool running = SDL_TRUE;
+
+    while (running)
     {
         SDL_Event e;
-        while (SDL_PollEvent(&e)) {}
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_q)
+            {
+                running = SDL_FALSE;
+            }
+            else if (e.type == SDL_QUIT)
+            {
+                running = SDL_FALSE;
+            }
+        }
+
         SDL_Delay(16);
     }
+
 
     fclose(image);
     SDL_DestroyWindow(window);
