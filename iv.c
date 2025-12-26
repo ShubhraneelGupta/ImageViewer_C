264d817ea988504871b9ba9b2c7f1af1c874f963
Shubhraneel Gupta <129332238+ShubhraneelGupta@users.noreply.github.com>
2025-12-26 12:23:08 +0530

    Takes file name as cmd input
    -- rejects anything other than ppm
    -- also added the logic to extract file extension, so supporting other extensions will be easier in future

--- iv.c
+++ iv.c
@@ -1,16 +1,75 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <string.h>
 
-#define WIDTH 900
-#define HEIGHT 600
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    //File handling
+    FILE *image;
+
+    if(argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    //Extracting file name and extension.
+    char fileName[128];
+    char fileExt[16];
+    sscanf(argv[1], "%s", &fileName);
+
+    char *dot = strrchr(fileName, '.');
+    if (dot && dot[1] != '\0') 
+    {
+        strncpy(fileExt, dot + 1, sizeof(fileExt) - 1);
+        fileExt[sizeof(fileExt) - 1] = '\0';
+    } else 
+    {
+        fileExt[0] = '\0'; 
+    }
+
+    //Only works for .ppm file format currently.
+    if(strcmp("ppm\0", fileExt))
+    {
+        fprintf(stderr, "Only supports .ppm for now.\n");
+        return 1;
+    }
+
+
+//PPM
+    image = fopen(argv[1], "r");
+    if (image == NULL) 
+    {
+        perror("fopen");
+        return 1;
+    }
+    //Finding height and width
+    char height[8];
+    char width[8];
+
+    //Height and width are in 3rd line for ppm files
+    char buf[256]; int line = 0;
+    while(fgets(buf, sizeof buf, image))
+    {
+        line++;
+        if(line == 3)
+        {
+            sscanf(buf, "%s %s", &width, &height);
+            break;
+        }
+    }
+
+    //converting height and width to int
+    const int HEIGHT = atoi(height);
+    const int WIDTH = atoi(width);
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+    {
         fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
         return 1;
     }
 
+
+    //Window creation.
     SDL_Window *window = SDL_CreateWindow(
         "Test Window",
         SDL_WINDOWPOS_CENTERED,
@@ -19,7 +78,8 @@ int main(void)
         HEIGHT,
         0
     );
-    if (!window) {
+    if (!window) 
+    {
         fprintf(stderr, "CreateWindow failed: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
@@ -64,7 +124,8 @@ int main(void)
 
 //CPU WORKFLOW
     Uint8 r, g, b;
-    SDL_Rect pixel = (SDL_Rect){
+    SDL_Rect pixel = (SDL_Rect)
+    {
             0, 0,
             1, 1
     };
@@ -86,12 +147,14 @@ int main(void)
     // Event polling is required on Wayland to keep the surface mapped.
     // on X11 the window may still appear without it, but polling is still recommended.
     Uint32 start = SDL_GetTicks();
-    while (SDL_GetTicks() - start < 2000) {
+    while (SDL_GetTicks() - start < 2000) 
+    {
         SDL_Event e;
         while (SDL_PollEvent(&e)) {}
         SDL_Delay(16);
     }
 
+    fclose(image);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
